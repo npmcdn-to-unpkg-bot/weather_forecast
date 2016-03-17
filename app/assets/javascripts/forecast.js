0cc9277767f7fd692414f65a8ad34b6d35acde05
Tigran Grigoryan <tigran.gr@gmail.com>
2016-03-17 20:04:21 +0400

    Modified the suggestion box behaviour and made appropriate changes in geo_controller

--- app/assets/javascripts/forecast.js
+++ app/assets/javascripts/forecast.js
@@ -1,36 +1,105 @@
 $(document).ready(function() {
 // Get your data source
-    var dataSource = new Bloodhound({
+    var dataSourceZipcodes = new Bloodhound({
         datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
         queryTokenizer: Bloodhound.tokenizers.whitespace,
-        prefetch: 'geo/all',
+        //prefetch: 'geo/all',
         remote: {
-            url: 'geo/lookup?query=%QUERYSTRING',
+            url: 'geo/lookup?type=zipcode&query=%QUERYSTRING',
+            wildcard: '%QUERYSTRING'
+        }
+    });
+
+    var dataSourceCities = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        //prefetch: 'geo/all',
+        remote: {
+            url: 'geo/lookup?type=city&query=%QUERYSTRING',
+            wildcard: '%QUERYSTRING'
+        }
+    });
+
+    var dataSourceStates = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        //prefetch: 'geo/all',
+        remote: {
+            url: 'geo/lookup?type=state&query=%QUERYSTRING',
             wildcard: '%QUERYSTRING'
         }
     });
 
 // initialize your element
-    var $typeahead = $('#search-box .typeahead').typeahead(null, {
-        display: 'city',
-        source: dataSource,
-        limit: 10,
-        templates: {
-            empty: [
-                '<div class="empty-message">',
-                'unable to find any location that match the current query',
-                '</div>'
-            ].join('\n'),
-            suggestion: function(data) {
-                var result = '<div>';
-                if (data.zipcode) {
-                    result += '<strong>' + data.zipcode + '</strong>, ';
-                }
-                result += '<strong>' + data.city  + '</strong>, <strong>' + data.state + '</strong>(state)</div>';
-                return result;
+    var $typeahead = $('#search-box .typeahead').typeahead(
+        {
+            //highlight: true
+        },
+        {
+            display: 'zipcode',
+            name: 'zipcodes',
+            source: dataSourceZipcodes,
+            limit: 10,
+            templates: {
+                /*empty: [
+                    '<div class="empty-message">',
+                    'No zipcodes found',
+                    '</div>'
+                ].join('\n'),*/
+                suggestion: function(data) {
+                    var result = '<div>';
+                    if (data.zipcode) {
+                        result += '<strong>' + data.zipcode + '</strong>, ';
+                    }
+                    result += '<strong>' + data.city  + '</strong>, <strong>' + data.state + '</strong>(state)</div>';
+                    return result;
+                },
+                header: '<h3 class="suggestion-heading">Zipcodes</h3>'
+            }
+        },
+        {
+            display: 'city',
+            name: 'cities',
+            source: dataSourceCities,
+            templates: {
+                /*empty: [
+                    '<div class="empty-message">',
+                    'No cities found',
+                    '</div>'
+                ].join('\n'),*/
+                suggestion: function(data) {
+                    var result = '<div>';
+                    if (data.zipcode) {
+                        result += '<strong>' + data.zipcode + '</strong>, ';
+                    }
+                    result += '<strong>' + data.city  + '</strong>, <strong>' + data.state + '</strong>(state)</div>';
+                    return result;
+                },
+                header: '<h3 class="suggestion-heading">Cities</h3>'
+            }
+        },
+        {
+            display: 'state',
+            name: 'states',
+            source: dataSourceStates,
+            templates: {
+                /*empty: [
+                    '<div class="empty-message">',
+                    'No states found',
+                    '</div>'
+                ].join('\n'),*/
+                suggestion: function(data) {
+                    var result = '<div>';
+                    if (data.zipcode) {
+                        result += '<strong>' + data.zipcode + '</strong>, ';
+                    }
+                    result += '<strong>' + data.city  + '</strong>, <strong>' + data.state + '</strong>(state)</div>';
+                    return result;
+                },
+                header: '<h3 class="suggestion-heading">States</h3>'
             }
         }
-    });
+    );
 
     /*$('#search-box .typeahead').typeahead({
      hint: true,
--- app/controllers/geo_controller.rb
+++ app/controllers/geo_controller.rb
@@ -3,10 +3,21 @@ class GeoController < ApplicationController
 
   def lookup
     if params[:query]
+=begin
       if /^[0-9]*$/.match(params[:query])
         geodata = Geodatum.select("zipcode, state, city").where('zipcode LIKE ?', "#{params[:query]}%")
       else
         geodata = Geodatum.select("state, city").where('state LIKE ? OR city LIKE ?', "#{params[:query]}%", "#{params[:query]}%").distinct(true)
+      end
+=end
+
+      geodata = case params[:type]
+        when 'zipcode'
+          Geodatum.select("zipcode, state, city").where('zipcode LIKE ?', "#{params[:query]}%")
+        when 'city'
+          Geodatum.select("state, city").where('city LIKE ?', "#{params[:query]}%").distinct(true)
+        when 'state'
+          Geodatum.select("state, city").where('state LIKE ?', "#{params[:query]}%").distinct(true)
       end
 
       render json: geodata
